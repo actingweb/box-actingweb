988617d173b0fc5afb9fe6c0c07daffa69f9536f
Greger Wedel <wedel@cisco.com>
2016-11-01 13:20:23 +0100

    Support for retrieving folders from db, cleaning up (leave Box folder), and create shard link

--- box/box.py
+++ box/box.py
@@ -35,9 +35,9 @@ class box():
         self.actorId = actorId
         self.auth = auth
         self.box = {
-            'folder_uri': "https://api.box.com/2.0/folders",
-            'collaboration_uri': "https://api.box.com/2.0/collaborations",
-            'webhook_uri': "https://api.box.com/2.0/webhooks",
+            'folder_uri': "https://api.box.com/2.0/folders/",
+            'collaboration_uri': "https://api.box.com/2.0/collaborations/",
+            'webhook_uri': "https://api.box.com/2.0/webhooks/",
         }
 
     def lastResponse(self, what=''):
@@ -50,16 +50,123 @@ class box():
             'message': self.auth.oauth.last_response_message,
         }
 
+    def cleanupAllFolders(self):
+        folders = Folder.query(Folder.actorId == self.actorId
+                    ).fetch(use_cache=False)
+        if not folders:
+            return False
+        for folder in folders:
+            self.cleanupFolder(folder_id=folder.boxId)
+        return True
+
+    def cleanupFolder(self, folder_id=None):
+        """ Delete webhook @ box, just clean db for folder & collabs."""
+        if not folder_id:
+            return False
+        self.deleteWebhook(folder_id=folder_id)
+        folder = Folder.query(Folder.actorId == self.actorId,
+                                Folder.boxId == folder_id
+                                ).get(use_cache=False)
+        if not folder:
+            return False
+        folder.key.delete(use_cache=False)
+        collabs = Collaboration.query(
+                    Collaboration.actorId == self.actorId,
+                    Collaboration.folderId == folder_id
+                    ).fetch(use_cache=False)
+        for collab in collabs:
+            collab.key.delete(use_cache=False)
+        return True
+
+    def getFolders(self):
+        folders = Folder.query(Folder.actorId == self.actorId
+                    ).fetch(use_cache=False)
+        if not folders:
+            return None
+        ret = []
+        for folder in folders:
+            ret.append(self.getFolder(folder.boxId))
+        return ret
+
+    def getFolder(self, name=None, folder_id=None, parent='0'):
+        if not name and not folder_id:
+            return None
+        if folder_id:
+            folder = Folder.query(Folder.actorId == self.actorId,
+                                  Folder.boxId == folder_id,
+                                  Folder.parentId == parent
+                                  ).get(use_cache=False)
+        else:
+            folder = Folder.query(Folder.actorId == self.actorId,
+                                  Folder.name == name,
+                                  Folder.parentId == parent
+                                  ).get(use_cache=False)
+        if not folder:
+            return False
+        ret = {
+            'boxId': folder.boxId,
+            'name' : folder.name,
+            'parentId' : folder.parentId,
+            'url' : folder.url,
+        }
+        collabs = Collaboration.query(
+                    Collaboration.actorId == self.actorId,
+                    Collaboration.folderId == ret['boxId']
+                    ).fetch(use_cache=False)
+        ret['users'] = []
+        for collab in collabs:
+            ret['users'].append({
+                'email': collab.email,
+                'collabBoxId': collab.boxId,
+                'userBoxId': collab.userId,
+                'role': collab.role,
+            })
+        hook = Webhook.query(
+                Webhook.actorId == self.actorId,
+                Webhook.folderId == ret['boxId']
+                ).get(use_cache=False)
+        if hook:
+            ret['webhookId'] = hook.boxId
+        return ret
+
+    def createLink(self, folder_id=None, level='collaborators'):
+        """ Creates a box folder shared link and returns url or None """
+        if not folder_id:
+            return None
+        folder = Folder.query(Folder.actorId == self.actorId,
+                              Folder.boxId == folder_id,
+                              ).get(use_cache=False)
+        if not folder:
+            return None
+        params = {
+            'shared_link': {'access': level},
+        }
+        ret = self.auth.oauthPUT(url=self.box['folder_uri'] + folder_id, params=params)
+        if self.lastResponse('code') != 200:
+            return None
+        if 'shared_link' in ret and ret['shared_link'] and 'url' in ret['shared_link']:
+            folder.url = ret['shared_link']['url']
+            folder.put(use_cache=False)
+            return folder.url
+        else:
+            return None
+
     def createFolder(self, name=None, parent='0'):
         """ Creates a box folder and returns the box id or None """
         if not name:
+            return None
+        if self.getFolder(name=name, parent=parent):
+            self.auth.oauth.last_response_code = 409
+            self.auth.oauth.last_response_message = 'The folder already exists.'
             return None
         params = {
             'name': name,
             'parent': {'id': parent},
         }
         ret = self.auth.oauthPOST(url=self.box['folder_uri'], params=params)
-        if self.lastResponse('code') != 201:
+        if self.lastResponse('code') == 409:
+            self.auth.oauth.last_response_message = 'The folder already exists.'
+        if self.lastResponse('code') != 201 or not ret:
             return None
         if 'shared_link' in ret and ret['shared_link'] and 'url' in ret['shared_link']:
             url = ret['shared_link']['url']
@@ -72,6 +179,23 @@ class box():
                           url=url)
         dbfolder.put()
         return ret['id']
+
+    def deleteCollaboration(self, folder_id=None, email=None):
+        if not folder_id or not email:
+            return False
+        collab = Collaboration.query(
+                    Collaboration.actorId == self.actorId,
+                    Collaboration.email == email,
+                    Collaboration.folderId == folder_id).get()
+        if not collab:
+            return False
+        ret = self.auth.oauthDELETE(url=self.box['collaboration_uri'] + collab.boxId)
+        if self.lastResponse('code') != 204:
+            logging.debug('Unable to delete collaboration for user(' +
+                          email + ') on folder(' + folder_id + ')')
+            return False
+        collab.key.delete()
+        return True
 
     def createCollaboration(self, folder_id=None, email=None, role=None, 
                             notify=None):
@@ -121,6 +245,21 @@ class box():
                                             role=role, notify=notify):
                 logging.warn('Was not able to add ' + email +
                              ' to access list for folder(' + folder_id + ')')
+
+    def deleteWebhook(self, folder_id=None):
+        if not folder_id:
+            return False
+        hook = Webhook.query(
+                Webhook.actorId == self.actorId,
+                Webhook.folderId == folder_id).get()
+        if not hook:
+            return False
+        ret = self.auth.oauthDELETE(url=self.box['webhook_uri'] + hook.boxId)
+        if self.lastResponse('code') != 204:
+            logging.warn('Failed to delete webhook for folder(' + folder_id + ')')
+            return False
+        hook.key.delete()
+        return True
 
     def createWebhook(self, folder_id=None, triggers=None, callback=None):
         if not folder_id or not callback:
