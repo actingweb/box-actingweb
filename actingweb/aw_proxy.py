559b15edef96f738a6ac39467e6877e1b2605b84
Greger Wedel <wedel@cisco.com>
2016-11-01 13:33:09 +0100

    Latest aw_proxy.py support from actingweb lib.

--- actingweb/aw_proxy.py
+++ actingweb/aw_proxy.py
@@ -84,6 +84,82 @@ class aw_proxy():
         try:
             result = json.loads(response.content)
         except:
-            logging.warn("Not able to parse response when creating resource at(" + url + ")")
-            result = None
+            logging.debug("Not able to parse response when creating resource at(" + url + ")")
+            result = {}
         return result
+
+    def changeResource(self, path=None, params=None):
+        if not path or len(path) == 0:
+            return None
+        if not params:
+            params = {}
+        if not self.trust or not self.trust.baseuri or not self.trust.secret:
+            return None
+        data = json.dumps(params)
+        headers = {'Authorization': 'Bearer ' + self.trust.secret,
+                   'Content-Type': 'application/json',
+                   }
+        url = self.trust.baseuri.strip('/') + '/' + path.strip('/')
+        logging.debug(
+            'Changing trust peer resource at (' + url + ') with data(' +
+            str(data) + ')')
+        try:
+            response = urlfetch.fetch(url=url,
+                                      method=urlfetch.PUT,
+                                      payload=data,
+                                      headers=headers
+                                      )
+            self.last_response_code = response.status_code
+            self.last_response_message = response.content
+        except:
+            logging.debug('Not able to change peer resource')
+            self.last_response_code = 408
+            return {
+                'error': {
+                    'code': 408,
+                    'message': 'Unable to communciate with trust peer service.',
+                },
+            }
+        logging.debug('Change trust peer resource PUT response:(' +
+                      str(response.status_code) + ') ' + response.content)
+        if response.status_code < 200 or response.status_code > 299:
+            logging.warn('Not able to change trust peer resource.')
+        try:
+            result = json.loads(response.content)
+        except:
+            logging.debug("Not able to parse response when changing resource at(" + url + ")")
+            result = {}
+        return result
+
+    def deleteResource(self, path=None):
+        if not path or len(path) == 0:
+            return None
+        if not self.trust or not self.trust.baseuri or not self.trust.secret:
+            return None
+        headers = {'Authorization': 'Bearer ' + self.trust.secret,
+                   }
+        url = self.trust.baseuri.strip('/') + '/' + path.strip('/')
+        logging.debug(
+            'Deleting trust peer resource at (' + url + ')')
+        try:
+            response = urlfetch.fetch(url=url,
+                                      method=urlfetch.DELETE,
+                                      headers=headers
+                                      )
+            self.last_response_code = response.status_code
+            self.last_response_message = response.content
+        except:
+            logging.debug('Not able to delete peer resource')
+            self.last_response_code = 408
+            return {
+                'error': {
+                    'code': 408,
+                    'message': 'Unable to communciate with trust peer service.',
+                },
+            }
+        logging.debug('Delete trust peer resource POST response:(' +
+                      str(response.status_code) + ') ' + response.content)
+        if response.status_code < 200 or response.status_code > 299:
+            logging.warn('Not able to delete trust peer resource.')
+            return False
+        return True
